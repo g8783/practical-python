9bfdbb4a319badf9bfe87b3ea9abd382bf5fc498
g8783 <g8783@outlook.com>
2020-07-01 19:46:59 -0400

    1.2 complete

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height_i = 100
+constant_bounce = 0.6
+height_b = height_i 
+i = 0
+while i < 10:
+    height_b = height_b * constant_bounce
+    i = i + 1
+    print(i, round(height_b,4))
