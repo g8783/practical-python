13cd01a9a9c2bca132132ea15c73e3273c2079bc
g8783 <g8783@outlook.com>
2020-07-15 10:35:20 -0400

    2.2

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    'Reads portfolio file into a list of tuples'
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            line = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(line)
+
+    return portfolio
+
